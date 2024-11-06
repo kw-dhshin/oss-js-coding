960a108964cc28761a94892c7c323eb471f0dd52
jhchoi <ulra26@naver.com>
2024-11-06 11:44:39 +0900

    solve problem1

--- problem1.js
+++ problem1.js
@@ -17,6 +17,11 @@ For n = 5, the output should be:
 function printTriangle(n) {
 
     // TODO: Implement this function
+    for (let i = 1; i <= n; i++) {
+        // Create a string with n - i spaces followed by i asterisks
+        let row = ' '.repeat(n - i) + '*'.repeat(i);
+        console.log(row);
+    }
 
 }
 

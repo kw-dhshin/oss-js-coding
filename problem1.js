c00ab8fe11806edb3c3fc315fac4423de84a47d5
sumin-absm <jangsumin21@gmail.com>
2024-11-06 10:34:17 +0900

    complete printTriangle

--- problem1.js
+++ problem1.js
@@ -1,23 +1,15 @@
-/*
-(Problem)
-Write a JavaScript function that, given a positive integer n,
-prints a right-aligned triangle pattern of asterisks (*).
-Each row of the triangle should contain an increasing number of asterisks,
-starting with 1 asterisk at the top and ending with n asterisks at the bottom.
-
-(Example)
-For n = 5, the output should be:
-    *
-   **
-  ***
- ****
-*****
-*/
-
 function printTriangle(n) {
 
-    // TODO: Implement this function
-
+for(let i = 1; i <= 5; i++){
+	let stars = "";
+	for(let j = 1; j <= i; j++){ 
+		stars = stars + " ";
+	}
+	for(let j=5;j>=i;j--){
+		stars = stars +"*";
+	}
+	console.log(stars);
+}
 }
 
 // Example usage:

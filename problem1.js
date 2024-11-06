531cc58e7f7d9cb13e10e6aaf07d581bde9e833c
sojung0628 <ksj020628@naver.com>
2024-11-06 10:26:13 +0900

    add code

--- problem1.js
+++ problem1.js
@@ -15,9 +15,19 @@ For n = 5, the output should be:
 */
 
 function printTriangle(n) {
-
-    // TODO: Implement this function
-
+    for (let i = 1; i <= n; i++) {
+        let row = "";
+        
+        for (let j = 0; j < n - i; j++) {
+            row += " ";
+        }
+        
+        for (let k = 0; k < i; k++) {
+            row += "*";
+        }
+        
+        console.log(row);
+    }
 }
 
 // Example usage:
